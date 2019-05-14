5ce3f4bc857420ea14e0d061d23030a491b483cd
QuinB6248 <4_sAfJO25N@outlook.com>
2019-05-14 10:10:12 +0200

    showing images with redux

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 export default class Detailpage extends Component {
-  render() {
+  
+  render() { 
+    const dogImages  = this.props.dogImages
+    
     return (
       <div>
         <Link to="/">Go back to the index</Link>
+      
+
+
+        { dogImages && this.props.dogImages.map(url => url.map(image => <img key={image} src= { image } alt = "Dog"/>)) }
+        { !dogImages && 'Loading...'}
       </div>
     )
   }
 }
+
