5b4d03b09ace2dc26e4b51359402625872c348f1
ANDREA VAN LIERE <andreavanliere@yahoo.com>
2019-05-14 12:15:49 +0200

    Game1 page and random image

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import DogsListContainer from './components/DogsListContainer';
 import { Route } from 'react-router-dom'
 import DetailPageContainer from './components/DetailPageContainer';
+import Game1Container from './components/Game1Container';
 
 function App() {
   return (
     <div className="App">
      <Route exact path="/" component={DogsListContainer} />
      <Route path="/detailpage/:breed" component={DetailPageContainer} />
+     <Route exact path="/game1/" component={Game1Container} />
     </div>
   );
 }
