6c99f85360657ed7a590e6ef7eedb84e785fe5cb
AidaRos95 <aidajros@gmail.com>
2019-05-14 17:37:39 +0200

    Options are checked whether correct or wrong, reducer adjusted for game 1

--- src/components/Game1Container.js
+++ src/components/Game1Container.js
@@ -5,34 +5,37 @@ import Game1 from './Game1';
 
 class Game1Container extends Component {
   componentDidMount() {
+    
     request
       .get('https://dog.ceo/api/breeds/image/random')
       .then(response => {
-        console.log(response.body.message)
+        console.log("BODY:", response.body)
         const randomDogImg = response.body.message
           this.props.dispatch({
           type: 'SHOW_RANDOM_IMAGE',
-          payload: randomDogImg
+          payload: {
+            image: randomDogImg,
+            id: 'correct'
+          }
         })
       })
       .catch(console.error)
   }
-
-  // const urlSplit = this.props.dogRandomImage; 
-  // const dogName = urlSplit.split('/')[3]
-
+  
   render() {
+    
+      // const urlSplit = this.props.dogRandomImage; 
+      // const dogName = urlSplit.split('/')[3]
+      // console.log(dogName)
     return (
     <div>
-         <p>Question Time!</p> 
-          <Game1 /> 
-          <br />
-          <img src={this.props.dogRandomImage} alt="random"></img>     
+          <Game1 dogRandomImage={this.props.dogRandomImage}/> 
     </div>
   )}
 }
 
 const mapStateToProps = (state) => {
+  console.log("STATE", state.dogRandomImage)
   return {
       dogRandomImage: state.dogRandomImage
   }
--- src/components/ProgressBarContainer.js
+++ src/components/ProgressBarContainer.js
@@ -9,16 +9,16 @@ export default class ProgressBarContainer extends Component {
     
     // Function to check whether answer is correct
     // Selected option is coming from an 'onSubmit' or 'onClick' event    
-    checkAnswer = (selectedOption) => {
-        if (selectedOption === true) {
-            const actionTrue = {
-                type: 'CHECK_ANSWER',
-                payload: true
-            }
-        }
-        this.setState(this.state.total++)
-        console.log("CORRECT:", this.state.correct, "TOTAL:", this.state.total)
-    }
+    // checkAnswer = (selectedOption) => {
+    //     if (selectedOption === true) {
+    //         const actionTrue = {
+    //             type: 'CHECK_ANSWER',
+    //             payload: true
+    //         }
+    //     }
+    //     this.setState(this.state.total++)
+    //     console.log("CORRECT:", this.state.correct, "TOTAL:", this.state.total)
+    // }
     
     render () {
         const successRate = (this.state.correct / this.state.total) * 100
