711e0f011409f676908cfc13041ab39d680295b5
AidaRos95 <aidajros@gmail.com>
2019-05-15 11:57:46 +0200

    Game 1 options randomized

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as request from 'superagent';
 
 export default class Game1 extends Component {
 
   checkAnswer = (event) => {
-    if (event.target.id === this.props.dogRandomImage.id){  
+    if (event.target.name === this.props.dogRandomImage.breed){  
       console.log("ANSWER WAS TRUE!")
       const correct = {
         type: 'ANSWERS',
@@ -24,33 +25,35 @@ export default class Game1 extends Component {
   }
 
   render() {
-    console.log('Image in presentation:', this.props.dogRandomImage)
-
+    const correctBreed = this.props.dogRandomImage.breed
+    const { randomBreeds } = this.props
+    
     return (
       <div>
         <h1>What is the breed of the image below?</h1>
                   
           <br />
-          <img src={this.props.dogRandomImage.image} alt="random"></img>   
+          <img src={this.props.dogRandomImage.url} alt="random"></img>   
 
         <div>
-          <input onClick={this.checkAnswer} type="radio" id="correct" name="dogbreed" value="Option A" />
-          <label for="Option A">Option A</label>
+          <input onClick={this.checkAnswer} type="radio" id="correct" name={correctBreed} value="Option A" />
+          <label for="Option A">{correctBreed}</label>
         </div>
 
         <div>
-          <input onClick={this.checkAnswer} type="radio" id="false" name="dogbreed" value="Option B" />
-          <label for="Option B">Option B</label>
+          <input onClick={this.checkAnswer} type="radio" id="false" name={randomBreeds && randomBreeds[0]} value="Option B" />
+          <label for="Option B">{randomBreeds && randomBreeds[0]}</label>
         </div>
 
         <div>
-          <input onClick={this.checkAnswer} type="radio" id="false" name="dogbreed" value="Option C" />
-          <label for="Option C">Option C</label>
+          <input  type="radio" id="false" name={randomBreeds && randomBreeds[1]} value="Option C" />
+          <label for="Option C">{randomBreeds && randomBreeds[1]}</label>
         </div>
         <br />
 
-        {/* <button>Answer Question!</button> */}
-        {/* ^^^ Should dispatch an action or call a function dispatching an action */}
+        
+        {/* <button onClick={this.checkAnswer}>Answer Question!</button> */}
+        {/* ^^^ Should dispatch an action or call a function dispatching an action*/}
         
         <br/>
         <Link to="/">Go back to the index</Link>
@@ -59,3 +62,4 @@ export default class Game1 extends Component {
       )
     }
 }
+
